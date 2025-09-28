fbcdb479396a4ee39ca45efdb0b5adb9e82d5952
Mz Wajih <mz.wajih@Mzs-MacBook-Pro.local>
2025-09-28 21:05:19 +0200

    Ajout test addXp retourne true - couvre branche level up réussie

--- src/test/java/re/forestier/edu/UpdatePlayerTests.java
+++ src/test/java/re/forestier/edu/UpdatePlayerTests.java
@@ -1,11 +1,16 @@
 package re.forestier.edu;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import java.util.ArrayList;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import re.forestier.edu.rpg.UpdatePlayer;
+import re.forestier.edu.rpg.player;
 
 public class UpdatePlayerTests {
     @Test
@@ -13,5 +18,15 @@ public class UpdatePlayerTests {
     void testUpdatePlayerConstructor() {
         UpdatePlayer updatePlayer = new UpdatePlayer();
         assertNotNull(updatePlayer);
+    }
+
+    @Test
+    @DisplayName("addXp doit retourner true quand le joueur monte de niveau")
+    void addXp_quandJoueurMonteNiveau_retourneTrue() {
+        player p = new player("T", "A", "ADVENTURER", 0, new ArrayList<>());
+        boolean result = UpdatePlayer.addXp(p, 10);
+        assertThat(result, is(true));
+        assertThat(p.getXp(), is(10));
+        assertThat(p.retrieveLevel(), is(2));
     }
 }
