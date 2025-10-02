3b07c62d684b9e0c8fe7409204983c34798c0815
Mz Wajih <mz.wajih@Mzs-MacBook-Pro.local>
2025-10-02 15:08:24 +0200

    Ajout test DWARF Holy Elixir - vérifie double bonus HP

--- src/test/java/re/forestier/edu/UpdatePlayerTests.java
+++ src/test/java/re/forestier/edu/UpdatePlayerTests.java
@@ -93,4 +93,16 @@ public class UpdatePlayerTests {
         UpdatePlayer.majFinDeTour(p);
         assertThat(p.currenthealthpoints, is(11)); // +1 seulement
     }
+
+    @Test
+    @DisplayName("majFinDeTour DWARF HP < 50% avec Holy Elixir - double bonus")
+    void majFinDeTour_dwarfAvecHolyElixir_doubleBonus() {
+        player p = new player("T", "A", "DWARF", 100, new ArrayList<>());
+        p.healthpoints = 40;
+        p.currenthealthpoints = 10; // < 20 (moitié)
+        p.inventory.add("Holy Elixir");
+        
+        UpdatePlayer.majFinDeTour(p);
+        assertThat(p.currenthealthpoints, is(12)); // +1 (classe) +1 (objet)
+    }
 }
