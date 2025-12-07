39cda7426484f59e22acdb5d7f8fbacfa750a4b7
Mz Wajih <mz.wajih@Mzs-MacBook-Pro.local>
2025-12-07 20:45:31 +0100

    refactor: créer classe Dwarf héritant d'AbstractPlayer

---
+++ src/main/java/re/forestier/edu/classes/Dwarf.java
@@ -0,0 +1,47 @@
+package re.forestier.edu.classes;
+
+import re.forestier.edu.rpg.AbstractPlayer;
+import re.forestier.edu.rpg.ITEM;
+import re.forestier.edu.rpg.STATS;
+
+import java.util.HashMap;
+
+public class Dwarf extends AbstractPlayer {
+    public Dwarf(String playerName, String avatarName, int maxHP, int money) {
+        super(playerName, avatarName, maxHP, money);
+        this.className = "Dwarf";
+        this.classDescription = "un nain commun, qu'on trouve à la pelle";
+    }
+
+    @Override
+    public void resolveEndOTurn() {
+        if (isKO()) {
+            System.out.println("Le joueur est KO !");
+            return;
+        }
+        if (getCurrentHealthPoints() < getMaxHealthPoints() / 2) {
+            // Vérifier si l'inventaire contient "Holy Elixir"
+            if (inventory.contains(ITEM.HolyElixir.toString())) {
+                addCurrentHealthPoints(2);
+            } else {
+                addCurrentHealthPoints(1);
+            }
+        }
+    }
+
+    @Override
+    public void initStats() {
+        HashMap<STATS, Integer[]> stats = new HashMap<>();
+        for (STATS stat : STATS.values()) {
+            stats.put(stat, new Integer[MAX_LEVEL]);
+        }
+        stats.put(STATS.INT, new Integer[]{1, 1, 1, 1, 1, 1, 1, 1, 1, 1});
+        stats.put(STATS.DEF, new Integer[]{0, 1, 1, 2, 2, 2, 2, 2, 2, 2});
+        stats.put(STATS.ATK, new Integer[]{3, 3, 4, 4, 4, 4, 4, 4, 4, 4});
+        stats.put(STATS.ALC, new Integer[]{4, 5, 5, 5, 5, 5, 5, 5, 5, 5});
+        stats.put(STATS.CHA, new Integer[]{0, 0, 0, 0, 1, 1, 1, 1, 1, 1});
+        stats.put(STATS.VIS, new Integer[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0});
+        this.statsPerLevel = stats;
+    }
+}
+
