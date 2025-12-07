3f3b10e2a2874480b36c90dc7fdfed6131bd755a
Mz Wajih <mz.wajih@Mzs-MacBook-Pro.local>
2025-12-07 20:48:54 +0100

    refactor: supprimer abilitiesPerTypeAndLevel(), stats gérées par classes

--- src/main/java/re/forestier/edu/rpg/UpdatePlayer.java
+++ src/main/java/re/forestier/edu/rpg/UpdatePlayer.java
@@ -4,9 +4,16 @@ import java.util.HashMap;
 
 public class UpdatePlayer {
 
+    /**
+     * Méthode temporaire pour compatibilité avec player.java
+     * Sera supprimée en Phase 6 quand player.java sera supprimé
+     * Retourne les stats du niveau 1 pour chaque classe
+     */
+    @Deprecated
     public static HashMap<String, HashMap<Integer, HashMap<String, Integer>>> abilitiesPerTypeAndLevel() {
-        HashMap<String, HashMap<Integer, HashMap<String, Integer>>> abilitiesPerTypeAndLevel = new HashMap<>();
-
+        HashMap<String, HashMap<Integer, HashMap<String, Integer>>> result = new HashMap<>();
+        
+        // ADVENTURER niveau 1
         HashMap<Integer, HashMap<String, Integer>> adventurerMap = new HashMap<>();
         HashMap<String, Integer> adventurerLevel1 = new HashMap<>();
         adventurerLevel1.put("INT", 1);
@@ -14,29 +21,9 @@ public class UpdatePlayer {
         adventurerLevel1.put("ATK", 3);
         adventurerLevel1.put("CHA", 2);
         adventurerMap.put(1, adventurerLevel1);
-
-        HashMap<String, Integer> adventurerLevel2 = new HashMap<>();
-        adventurerLevel2.put("INT", 2);
-        adventurerLevel2.put("CHA", 3);
-        adventurerMap.put(2, adventurerLevel2);
-
-        HashMap<String, Integer> adventurerLevel3 = new HashMap<>();
-        adventurerLevel3.put("ATK", 5);
-        adventurerLevel3.put("ALC", 1);
-        adventurerMap.put(3, adventurerLevel3);
-
-        HashMap<String, Integer> adventurerLevel4 = new HashMap<>();
-        adventurerLevel4.put("DEF", 3);
-        adventurerMap.put(4, adventurerLevel4);
-
-        HashMap<String, Integer> adventurerLevel5 = new HashMap<>();
-        adventurerLevel5.put("VIS", 1);
-        adventurerLevel5.put("DEF", 4);
-        adventurerMap.put(5, adventurerLevel5);
-
-        abilitiesPerTypeAndLevel.put("ADVENTURER", adventurerMap);
-
-
+        result.put("ADVENTURER", adventurerMap);
+        
+        // ARCHER niveau 1
         HashMap<Integer, HashMap<String, Integer>> archerMap = new HashMap<>();
         HashMap<String, Integer> archerLevel1 = new HashMap<>();
         archerLevel1.put("INT", 1);
@@ -44,54 +31,18 @@ public class UpdatePlayer {
         archerLevel1.put("CHA", 1);
         archerLevel1.put("VIS", 3);
         archerMap.put(1, archerLevel1);
-
-        HashMap<String, Integer> archerLevel2 = new HashMap<>();
-        archerLevel2.put("DEF", 1);
-        archerLevel2.put("CHA", 2);
-        archerMap.put(2, archerLevel2);
-
-        HashMap<String, Integer> archerLevel3 = new HashMap<>();
-        archerLevel3.put("ATK", 3);
-        archerMap.put(3, archerLevel3);
-
-        HashMap<String, Integer> archerLevel4 = new HashMap<>();
-        archerLevel4.put("DEF", 2);
-        archerMap.put(4, archerLevel4);
-
-        HashMap<String, Integer> archerLevel5 = new HashMap<>();
-        archerLevel5.put("ATK", 4);
-        archerMap.put(5, archerLevel5);
-
-        abilitiesPerTypeAndLevel.put("ARCHER", archerMap);
-
-
-        HashMap<Integer, HashMap<String, Integer>> dwarf = new HashMap<>();
+        result.put("ARCHER", archerMap);
+        
+        // DWARF niveau 1
+        HashMap<Integer, HashMap<String, Integer>> dwarfMap = new HashMap<>();
         HashMap<String, Integer> dwarfLevel1 = new HashMap<>();
         dwarfLevel1.put("ALC", 4);
         dwarfLevel1.put("INT", 1);
         dwarfLevel1.put("ATK", 3);
-        dwarf.put(1, dwarfLevel1);
-
-        HashMap<String, Integer> dwarfLevel2 = new HashMap<>();
-        dwarfLevel2.put("DEF", 1);
-        dwarfLevel2.put("ALC", 5);
-        dwarf.put(2, dwarfLevel2);
-
-        HashMap<String, Integer> dwarfLevel3 = new HashMap<>();
-        dwarfLevel3.put("ATK", 4);
-        dwarf.put(3, dwarfLevel3);
-
-        HashMap<String, Integer> dwarfLevel4 = new HashMap<>();
-        dwarfLevel4.put("DEF", 2);
-        dwarf.put(4, dwarfLevel4);
-
-        HashMap<String, Integer> dwarfLevel5 = new HashMap<>();
-        dwarfLevel5.put("CHA", 1);
-        dwarf.put(5, dwarfLevel5);
-
-        abilitiesPerTypeAndLevel.put("DWARF", dwarf);
-
-        return abilitiesPerTypeAndLevel;
+        dwarfMap.put(1, dwarfLevel1);
+        result.put("DWARF", dwarfMap);
+        
+        return result;
     }
 
     public static boolean addXp(player player, int xp) {
@@ -104,11 +55,8 @@ public class UpdatePlayer {
             // Give a random object
             player.inventory.add(ITEM.randomItem().toString());
 
-            // Add/upgrade abilities to player
-            HashMap<String, Integer> abilities = abilitiesPerTypeAndLevel().get(player.getAvatarClass()).get(newLevel);
-            abilities.forEach((ability, level) -> {
-                player.abilities.put(ability, abilities.get(ability));
-            });
+            // Les capacités sont maintenant gérées dynamiquement par les classes concrètes
+            // via getStatistic() dans AbstractPlayer. Plus besoin de mise à jour manuelle.
             return true;
         }
         return false;
